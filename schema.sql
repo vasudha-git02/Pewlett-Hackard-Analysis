cc3a7dc0d836d6f8bfc26ad75c9953784d140a88
vasudha-git02 <104597335+vasudha-git02@users.noreply.github.com>
2022-06-15 16:03:52 -0400

    adding schema.sql

---
+++ schema.sql
@@ -0,0 +1,60 @@
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE DEPARTMENTS(
+    dept_no varchar(4) NOT NULL,
+	dept_name varchar(40) NOT NULL,
+	PRIMARY KEY(dept_no),
+	UNIQUE (dept_name)
+);
+
+CREATE TABLE Employees(
+	emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_manager(
+	dept_no varchar(4) NOT NULL,
+	emp_no int NOT NULL,
+	from_date date NOT NULL,
+	to_date date NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES Employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES DEPARTMENTS (dept_no),
+PRIMARY KEY (emp_no,dept_no)
+);
+
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_emp(
+	emp_no int NOT NULL,
+	dept_no varchar(4) NOT NULL,
+	from_date date NOT NULL,
+	to_date date NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES Employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES DEPARTMENTS (dept_no),
+PRIMARY KEY (emp_no,dept_no)
+);
+
+
+CREATE TABLE titles(
+	emp_no int NOT NULL,
+	title varchar NOT NULL,
+	from_date date NOT NULL,
+	to_date date NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES Employees (emp_no),
+PRIMARY KEY (emp_no)
+);
+
+select * from departments;
+
+select * from information_schema.tables;
